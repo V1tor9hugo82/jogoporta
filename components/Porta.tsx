0feab71115cbfd0db3fc9dfa3f96ad555b1dca77
vitor <vhrocha17@gmail.com>
2021-12-03 09:31:22 -0300

    repositorio jogo da porta

---
+++ components/Porta.tsx
@@ -0,0 +1,41 @@
+import styles from "../styles/Porta.module.css"
+import PortaModel from '../model/porta'
+
+interface PortaProps {
+  value: PortaModel
+  onChange: (novaPorta: PortaModel) => void
+
+}
+
+export default function Porta(props: PortaProps) {
+  const Porta = props.value
+  const selecionada = Porta.selecionada && !Porta.aberta ? styles.selecionada : ''
+
+  const alternarSelecao = e => props.onChange(Porta.alternarSelecao())
+  const abrir = e => {
+    e.stopPropagation()
+    props.onChange(Porta.abrir())
+  }
+
+  function renderizarPorta() {
+    return (
+      <div className={styles.porta}>
+        <div className={styles.numero}>{Porta.numero}</div>
+        <div className={styles.macaneta}
+          onClick={abrir} ></div>
+      </div>
+
+    )
+  }
+
+  return (
+    <div className={styles.area} onClick={alternarSelecao}>
+      <div className={`${styles.estrutura} ${selecionada}`}>
+        {Porta.aberta ? false : renderizarPorta()}
+      </div>
+      <div className={styles.chao}></div>
+    </div>
+
+  )
+}
+
